5faa6f21ff76d4ea39de6bc4cc75a41a3ce7a23e
Farid Kurniawan <farid.kurniawan0412@gmail.com>
2025-10-25 16:59:53 +0700

    Implement MenuController and update menu view to display items

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class MenuController extends Controller
+{
+    public function index(Request $request)
+    {
+        $tableNumber = $request->query('meja');
+        if ($tableNumber) {
+            Session::put('tableNumber', $tableNumber);
+        }
+
+        $items = Item::where('is_active', 1)->orderby('name', 'asc')->get();
+
+        return view('customers.menu', compact('items', 'tableNumber'));
+    }
+}
--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -21,7 +21,13 @@ class ItemFactory extends Factory
             'category_id' => $this->faker->numberBetween(1, 2),
             'price' => $this->faker->randomFloat(2, 1000, 100000),
             'description' => $this->faker->text(),
-            'image' => $this->faker->imageUrl(),
+            'image' => fake()->randomElement([
+                'https://images.unsplash.com/photo-1612927601601-6638404737ce',
+                'https://images.unsplash.com/photo-1517701550927-30cf4ba1dba5',
+                'https://images.unsplash.com/photo-1515823064-d6e0c04616a7',
+                'https://images.unsplash.com/photo-1574484284002-952d92456975',
+                'https://images.unsplash.com/photo-1546069901-ba9599a7e63c',
+            ]),
             'is_active' => $this->faker->boolean(),
         ];
     }
