8304faae58f5bcb8fcd852daccfe21d42b1fafbf
Farid Kurniawan <farid.kurniawan0412@gmail.com>
2025-10-26 18:34:13 +0700

    menambahkan proses order dan memperbaiki migration

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 class MenuController extends Controller
 {
@@ -112,5 +116,75 @@ class MenuController extends Controller
         $tableNumber = Session::get('tableNumber');
 
         return view('customers.checkout', compact('cart', 'tableNumber'));
+    }
+
+    public function storeOrder(Request $request)
+    {
+        $cart = Session::get('cart');
+        $tableNumber = Session::get('tableNumber');
+        if (empty($cart)) {
+            return redirect()->route('cart')->with('error', 'Keranjang kosong. Silakan tambahkan pesanan terlebih dahulu.');
+        }
+
+        if (is_null($request->payment_method)) {
+            return redirect()
+                ->route('checkout')
+                ->with('error', 'Silakan pilih metode pembayaran terlebih dahulu.');
+        }
+
+
+        $validator = Validator::make($request->all(), [
+            'fullname' => 'required|string|max:255',
+            'phone' => 'required|string|max:15',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('checkout')->withErrors($validator);
+        }
+
+
+        $totalAmount = 0;
+        foreach ($cart as $item) {
+            $totalAmount += $item['price'] * $item['qty'];
+
+            $itemDetail = [
+                'item_id' => $item['id'],
+                'price' => (int) $item['price'] * ($item['price'] * 0.1),
+                'quantity' => $item['qty'],
+                'name' => substr($item['name'], 0, 50),
+            ];
+        }
+
+        $user = User::firstOrCreate([
+            'fullname' => $request->input('fullname'),
+            'phone' => $request->input('phone'),
+            'role_id' => 4
+        ]);
+
+        $order = Order::create([
+            'order_code' => 'ORD-' . $tableNumber . '-' . time(),
+            'user_id' => $user->id,
+            'subtotal' => $totalAmount,
+            'tax' => $totalAmount * 0.1,
+            'grand_total' => $totalAmount + ($totalAmount * 0.1),
+            'status' => 'pending',
+            'table_number' => $tableNumber,
+            'payment_method' => $request->payment_method,
+        ]);
+
+        foreach ($cart as $itemId => $item) {
+            OrderItem::create([
+                'order_id' => $order->id,
+                'item_id' => $item['id'],
+                'quantity' => $item['qty'],
+                'price' => $item['price'] * $item['qty'],
+                'tax' => $item['price'] * 0.1 * $item['qty'],
+                'total_price' => ($item['price'] * $item['qty']) + (0.1 * $item['qty'] * $item['price']),
+            ]);
+        }
+
+        Session::forget('cart');
+
+        return redirect()->route('menu')->with('success', 'Pesanan berhasil diproses. Terima kasih!');
     }
 }
